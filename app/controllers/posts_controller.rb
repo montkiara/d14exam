fb4d6aa5f374cbb8def90e5017ff27bf33d4c482
hiro <hiro@inkri.com>
2017-02-19 11:47:21 +0000

    till DIVE01

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(posts_params)
+    redirect_to posts_path
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :content)
+    end
+
+end
