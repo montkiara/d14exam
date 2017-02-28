fc1f2f329a3aefa486f1a7a04d04b184dddca8ca
hiro <hiro@inkri.com>
2017-02-28 12:33:12 +0000

    dive02 til before_action :set_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: {:edit, :update, :destroy}
+
   def index
     @posts = Post.all
   end
@@ -8,13 +10,40 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(posts_params)
-    redirect_to posts_path
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    #@post = Post.find(params[:id])
+  end
+
+  def update
+    #@post = Post.find(params[:id])
+    if @post.update(posts_params)
+      redirect_to posts_path, notice: "投稿を編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    #@post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました！"
   end
 
   private
     def posts_params
       params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 
 end
