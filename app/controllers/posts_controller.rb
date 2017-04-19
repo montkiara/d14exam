05976d7aaabf3937f19fa1b8fdcf7c68aef0fb16
hiro <hiro@inkri.com>
2017-04-19 09:44:44 +0000

    d11 sendmail on production

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     if @post.save
       redirect_to posts_path, notice: "投稿しました！"
-      NoticeMailer.sendmail_post(@post).deliver_now
+      NoticeMailer.sendmail_post(@post).deliver
     else
       render 'new'
     end
